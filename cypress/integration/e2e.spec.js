3a1ab18c288c4b60edb777e3de98ded51bbbb4d7
meskarin <rafael.ide@outlook.com>
2021-08-26 22:42:16 -0300

    Atividade módulo 12

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+const perfil = require('../fixtures/perfil.json')   
+
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     /*  Como cliente 
         Quero acessar a Loja EBAC 
@@ -10,12 +12,31 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         E validando minha compra ao final */
 
     beforeEach(() => {
-        cy.visit('/')
+        cy.visit('/minha-conta')
+        cy.fixture('perfil').then(dados =>{ 
+            cy.login(dados.usuario, dados.senha)
+            
+        })
+        
+        
+      
+    });
+
+    afterEach(() => {
+        cy.screenshot()
     });
 
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        //TODO 
-    });
+        cy.visit('/produtos')
+        
 
+        cy.addCarrinho("Abominable Hoodie", "M", "Blue", 2 )
+        cy.addCarrinho("Aero Daily Fitness Tee", "M", "Yellow", 3 )
+        cy.addCarrinho("Apollo Running Short", "33", "Black", 1 )
+        cy.addCarrinho("Ariel Roll Sleeve Sweatshirt", "M", "Purple", 2 )
+        
+        cy.checkout()
+
+    })
 
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,7 +23,9 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-
+import enderecoFaturamento from './page_objects/alterar-enderecos'
+const novoEndereco = require('../fixtures/novoEndereco.json')
+var faker = require('faker-br')
 
 Cypress.Commands.add('login', (usuario, senha) => {
     cy.get('#username').type(usuario)
@@ -31,3 +33,49 @@ Cypress.Commands.add('login', (usuario, senha) => {
     cy.get('.woocommerce-form > .button').click()
 });
 
+Cypress.Commands.add('registro', (email, password) =>{
+    cy.visit('/minha-conta')
+    cy.get('#reg_email').type(faker.internet.email)
+    cy.get('#reg_password').type(faker.internet.password)
+    cy.get(':nth-child(4) > .button').click()
+})
+
+
+Cypress.Commands.add('addCarrinho', (produto, tamanho, cor, quantidade) =>{
+    cy.get('#primary-menu > .menu-item-629 > a').click()
+    cy.get('[class="product-block grid"]')
+        .contains(produto)
+        .click()
+    cy.get('.button-variable-item-' + tamanho).click()
+    cy.get('.button-variable-item-' + cor).click()
+    cy.get('.input-text').clear().type(quantidade)
+    cy.get('.single_add_to_cart_button').click()     
+    cy.get('.woocommerce-message').should('contain',produto)
+})
+
+Cypress.Commands.add('checkout', (nome,sobrenome,empresa,pais,endereco,numero,cidade,estado,cep,telefone,email) =>{
+    cy.visit('/carrinho')
+    cy.get('.checkout-button').click()
+
+    //Chamar uma classe para alterar endereço de faturamento no checkout
+    enderecoFaturamento.checkoutFaturamento(
+        novoEndereco[0].nome,
+        novoEndereco[0].sobrenome,
+        novoEndereco[0].empresa,
+        novoEndereco[0].pais,
+        novoEndereco[0].endereco,
+        novoEndereco[0].numero,
+        novoEndereco[0].cidade,
+        novoEndereco[0].estado,
+        novoEndereco[0].cep,
+        novoEndereco[0].telefone,
+        novoEndereco[0].email
+    )
+
+
+    cy.get('#order_comments').type('Teste de pedido completo!')
+    cy.get('#terms').check()
+    cy.get('#place_order').click()
+    cy.get('#main').should('contain','Obrigado. Seu pedido foi recebido.')
+
+})
